de517e85f354c4f68834e513d51804236bcfb118
mdAshford <marcus@psydat.com>
2020-09-07 16:14:52 -0500

    + PyCallCoolProp.jl, and pB call to install it

---
+++ r/PyCallCoolProp.jl
@@ -0,0 +1,11 @@
+__precompile__() # this module is safe to precompile
+module PyCallCoolProp
+using PyCall
+
+const CoolProp = PyNULL()
+
+function __init__()
+    copy!(CoolProp, pyimport_conda("CoolProp.CoolProp", "CoolProp"))
+end
+
+end
